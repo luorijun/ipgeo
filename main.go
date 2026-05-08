c7b8073942870af23ac85c4e21cffc0384456dca
luorijun <luorijun@outlook.com>
2026-05-08 16:18:14 +0800

    完善 ip 解析与响应

--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"log/slog"
 	"net/http"
+	"net/netip"
 	"strings"
 
 	"github.com/lionsoul2014/ip2region/binding/golang/service"
 )
 
 func main() {
+	slog.SetLogLoggerLevel(slog.LevelDebug)
 
 	// 初始化查询服务
 	v4config, err := service.NewV4Config(service.BufferCache, "data/ip2region_v4.xdb", 20)
@@ -27,11 +31,57 @@ func main() {
 	}
 	defer server.Close()
 
+	getClientIP := func(ip string) (*IpInfo, error) {
+		result, err := server.SearchByStr(ip)
+		if err != nil {
+			slog.Debug(err.Error())
+			return nil, errors.New("failed to search IP info")
+		}
+
+		infos := strings.Split(result, "|")
+		if len(infos) < 4 {
+			slog.Debug("invalid IP info format: " + result)
+			return nil, errors.New("invalid IP info format")
+		}
+
+		return &IpInfo{
+			Ip:     ip,
+			Region: infos[0],
+			Prov:   infos[1],
+			City:   infos[2],
+			Isp:    infos[3],
+		}, nil
+	}
+
 	// 初始化 web 接口
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		bytes, err := json.Marshal(r.Header)
+		ip := r.Header.Get("X-Real-Ip")
+		if ip == "" {
+			ip = r.Header.Get("X-Forwarded-For")
+		}
+		if ip == "" {
+			addr, err := netip.ParseAddrPort(r.RemoteAddr)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte("Failed to parse client IP"))
+				slog.Debug(err.Error())
+				return
+			}
+			ip = addr.Addr().String()
+		}
+
+		geo, err := getClientIP(ip)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Failed to get IP info: " + err.Error()))
+			return
+		}
+
+		bytes, err := json.Marshal(geo)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Failed to marshal IP info"))
+			slog.Debug(err.Error())
 			return
 		}
 
@@ -40,31 +90,19 @@ func main() {
 
 	http.HandleFunc("/{ip}", func(w http.ResponseWriter, r *http.Request) {
 		ip := r.PathValue("ip")
-		if ip == "" {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
 
-		result, err := server.SearchByStr(ip)
+		geo, err := getClientIP(ip)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Failed to get IP info: " + err.Error()))
 			return
 		}
 
-		infos := strings.Split(result, "|")
-		if len(infos) < 4 {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		bytes, err := json.Marshal(map[string]string{
-			"region": infos[0],
-			"prov":   infos[1],
-			"city":   infos[2],
-			"isp":    infos[3],
-		})
+		bytes, err := json.Marshal(geo)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Failed to marshal IP info"))
+			slog.Debug(err.Error())
 			return
 		}
 
@@ -74,4 +112,12 @@ func main() {
 	if err := http.ListenAndServe(":8081", nil); err != nil {
 		panic(err)
 	}
+}
+
+type IpInfo struct {
+	Ip     string `json:"ip"`
+	Region string `json:"region"`
+	Prov   string `json:"prov"`
+	City   string `json:"city"`
+	Isp    string `json:"isp"`
 }
